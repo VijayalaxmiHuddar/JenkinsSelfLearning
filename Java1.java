3b741f0c5c384060a6c3b174079f8eff28bd44e9
Huddar <Vijayalaxmi_Huddar@Dell.com>
2021-10-19 23:28:36 +0530

    new
    new1

--- Java1.java
+++ Java1.java
@@ -1,24 +1,4 @@
-public class PalindromeProgram {
- 
-    public static void main(String[] args) {
- 
-        int rem, rev= 0, temp;
-    int n=121; // user defined number to be checked for palindrome 
- 
-        temp = n;
- 
-        // reversed integer is stored in variable 
-        while( n != 0 )
-        {
-            rem= n % 10;
-            rev= rev * 10 + rem;
-            n=n/10;
-        }
- 
-        // palindrome if orignalInteger(temp) and reversedInteger(rev) are equal
-        if (temp == rev)
-            System.out.println(temp + " is a palindrome.");
-        else
-            System.out.println(temp + " is not a palindrome.");
-    }
-}
+public class Simple{
+public static void main(String args[]){
+System.out.println("hello nethra");
+}}
